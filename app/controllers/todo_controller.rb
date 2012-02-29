f7380261c539aae97ce0fa257c054907c35fb7b2
Tom Rozendaal <tomrozendaal5434@gmail.com>
2012-02-29 22:23:47 +0100

    Implemented delete functionality

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,32 @@
 class TodoController < ApplicationController
   def index
+    @tasks = Task.all
+  end
+  def create
+    @params = params
+    
+    @task = Task.new(params)
+    @task.save
+    
+    redirect_to todo_index_path
+    
+  end
+  
+  def new
+    @priorities = {"High" => 1, "Medium" => 2, "Low" => 3}
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    @params = params
+    
+    @task = Task.find(params['id'])
+    @task.destroy
+    
+    redirect_to todo_index_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ Tomdo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  
+  resources :todo
    root :to => 'todo#index'
 
   # See how all your routes lay out with "rake routes"
